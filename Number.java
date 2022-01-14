cb5054ff9d118a7600576124211f2608a2d85b97
Fabi <66051462+FabiChan@users.noreply.github.com>
2022-01-13 21:29:20 -0600

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,10 @@
+//simple code to generate a random number for LHD Day 5 Challenge
+import java.io.*;
+import java.util.*;
+
+class Number {
+   public static void main(String args[]) {
+      double num = Math.random()*1000000;
+      System.out.println(num);
+   }
+}
